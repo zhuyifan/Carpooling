bf7f988a264e870e6b8f144f2b7339905b5b5300
Yudong Diao <ydiao@umass.edu>
2017-07-14 10:02:58 +0800

    merged

--- pages/passenger/passenger.js
+++ pages/passenger/passenger.js
@@ -1,18 +1,30 @@
 // pages/passenger/passenger.js
+var app = getApp();
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-  
+    userName: {},
+    nop: 0,
+    time: "",
+    destination: "",
+    bag: ""
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-  
+  onLoad: function () {
+      console.log('onLoad')
+      var that = this;
+      app.getUserInfo(function (userInfo) {
+        that.setData({
+          userInfo: userInfo
+        })
+        console.log(userInfo.nickName)
+      })
   },
 
   /**
