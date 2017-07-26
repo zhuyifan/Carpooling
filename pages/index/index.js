284798dd8e7de7fa7526d5aa1b38d88f5b40b74c
Ivanz7 <yifanzhu@umass.edu>
2017-07-26 16:00:20 -0400

    creat index framework

--- pages/index/index.js
+++ pages/index/index.js
@@ -10,6 +10,14 @@ Page({
     winHeight: 0,
     // tab切换  
     currentTab: 0,
+    items: [
+      { name:"刁豫东三", qidian:"BDL", date:"2017-8-9", time:"18:00", zhongdian:"Amherst", usertype:"人找车", seat:2},
+      { name: "刁豫东四", qidian: "Logan", date: "2017-7-9", time: "18:00", zhongdian: "Amherst", usertype: "人找车", seat: 2},
+      { name: "刁豫东五", qidian: "Boston", date: "2017-8-9", time: "18:00", zhongdian: "Amherst", usertype: "车找人", seat: 3},
+      { name: "刁豫东三", qidian: "Amherst", date: "2017-9-9", time: "18:00", zhongdian: "BDL", usertype: "车找人", seat: 2},
+      { name: "刁豫东一", qidian: "JFK", date: "2017-9-9", time: "18:00", zhongdian: "Amherst", usertype: "人找车", seat: 1},
+      { name: "刁豫东二", qidian: "Boston", date: "2017-10-9", time: "18:00", zhongdian: "Amherst", usertype: "车找人", seat: 2},
+    ]
   },
   onLoad: function () {
     var that = this;
