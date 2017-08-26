e17c0ed49af7f55a8f33b31043c1a8c28c7f33bd
Ivanz7 <yifanzhu@umass.edu>
2017-08-26 16:21:36 -0400

    henfan

--- pages/publish/publish.js
+++ pages/publish/publish.js
@@ -4,44 +4,73 @@ var app = getApp()
 var currentdate = app.globalData.date;
 var res;
 var len;
-var Diary = Bmob.Object.extend("event_data");
-var query = new Bmob.Query(Diary);
-query.equalTo("phone", app.globalData.phoneNum);
-query.find({
-  success: function (results) {
-    console.log("共查询到 " + results.length + " 条记录");
-    len = results.length;
-    // 循环处理查询到的数据
-    res = results;
-    for (var i = 0; i < results.length; i++) {
-      var object = results[i];
-      console.log(object.get('name') + ' - ' + object.get('phone'));
-    }
-  },
-  error: function (error) {
-    console.log("查询失败: " + error.code + " " + error.message);
-  }
-});
+
+
+
+  
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    items: [
-      {
-        id: 0, name: "川普", gender: "male", phone: "9177568000", wechat: "DTrump", qidian: "White House",
-        date: "2020-09-01", time: "7:00", zhongdian: "Congress", usertype: "车找人", mode: "Lincoln", seat: "3"
-      },
-    ],
+    phoneNum: "",
+    items: ""
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    var that = this;
+    wx.getStorage({
+      key: 'phoneNum',
+      success: function (res) {
+        // success
+        that.setData({
+          phoneNum: res.data
+        })
+      }
+    });
+
+    var Diary = Bmob.Object.extend("event_data");
+    var query = new Bmob.Query(Diary);
+    query.equalTo("phone", this.data.phoneNum);
+    query.find({
+      success: function (results) {
+        console.log("共查询到 " + results.length + " 条记录");
+        len = results.length;
+        // 循环处理查询到的数据
+        res = results;
+        for (var i = 0; i < results.length; i++) {
+          var object = results[i];
+          console.log(object.get('name') + ' - ' + object.get('phone'));
+        }
+      },
+      error: function (error) {
+        console.log("查询失败: " + error.code + " " + error.message);
+      }
+    });
+
+    for (var i = 0; i < len; i++) {
+      this.data.items = [{
+        id: res[i].get("id"), gender: res[i].get("gender"), name: res[i].get("name"),
+        phone: res[i].get("phone"), wechat: res[i].get("wechat"),
+        qidian: res[i].get("from"), date: res[i].get("date"), time: res[i].get("time"),
+        zhongdian: res[i].get("to"), usertype: res[i].get("person"), mode: res[i].get("mode"),
+        seat: res[i].get("seat")
+      }].concat(this.data.items),
+
+        this.setData({
+          items: this.data.items
+        })
+    }
   },
+
+    
+
+    
 
   /**
    * 生命周期函数--监听页面初次渲染完成
